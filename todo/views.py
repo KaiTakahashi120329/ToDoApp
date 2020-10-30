4c24fba0f15676921b9ffc9b5545d1edac1746ad
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-30 23:55:00 +0900

    detailviewの定義

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import ToDoModel
 
 class ToDoList(ListView):
     template_name = 'list.html'
+    model = ToDoModel
+
+class ToDoDetail(DetailView):
+    template_name = 'datail.html'
     model = ToDoModel
