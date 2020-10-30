02d3683f02ddd07b5ff0972f5d3ad28cf9bf2c55
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-30 23:54:51 +0900

    detailviewの定義

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ToDoList
+from .views import ToDoList, ToDoDetail
 
 
 urlpatterns = [
     path('list/', ToDoList.as_view()),
+    path('detail/<int:pk>/', ToDoDetail.as_view()),
 ]
