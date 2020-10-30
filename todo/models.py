1885f487cfaab586c63b4f9b5642d169ff372826
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-30 23:20:34 +0900

    modelの定義（仮）

--- todo/models.py
+++ todo/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class ToDoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
