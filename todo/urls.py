9fa01fbb37eced9117a9f3c6b7557df8313aea22
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-31 18:58:33 +0900

    新規投稿の機能追加

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ToDoList, ToDoDetail
+from .views import ToDoList, ToDoDetail, ToDoCreate
 
 
 urlpatterns = [
-    path('list/', ToDoList.as_view()),
-    path('detail/<int:pk>/', ToDoDetail.as_view()),
+    path('list/', ToDoList.as_view(), name='list'),
+    path('detail/<int:pk>/', ToDoDetail.as_view(), name='detail'),
+    path('create/', ToDoCreate.as_view(), name='create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import ToDoModel
+from django.urls import reverse_lazy
 
 class ToDoList(ListView):
     template_name = 'list.html'
@@ -9,3 +10,9 @@ class ToDoList(ListView):
 class ToDoDetail(DetailView):
     template_name = 'datail.html'
     model = ToDoModel
+
+class ToDoCreate(CreateView):
+    template_name = 'create.html'
+    model = ToDoModel
+    fields = ['title', 'memo', 'priority', 'duedate']
+    success_url = reverse_lazy('list')
