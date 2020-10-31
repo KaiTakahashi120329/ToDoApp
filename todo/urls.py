72f976efed0d26e4943c2d406e0bfbfe9d6c2605
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-31 19:17:07 +0900

    削除機能の追加

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ToDoList, ToDoDetail, ToDoCreate
+from .views import ToDoList, ToDoDetail, ToDoCreate, ToDoDelete
 
 
 urlpatterns = [
     path('list/', ToDoList.as_view(), name='list'),
     path('detail/<int:pk>/', ToDoDetail.as_view(), name='detail'),
     path('create/', ToDoCreate.as_view(), name='create'),
+    path('delete/<int:pk>/', ToDoDelete.as_view(), name='delete'),
 ]
