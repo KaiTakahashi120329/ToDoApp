997a20c3c246d237d075f71411d3baa6ab3f236d
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-31 19:23:22 +0900

    削除機能の追加

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import ToDoModel
 from django.urls import reverse_lazy
 
@@ -15,4 +15,9 @@ class ToDoCreate(CreateView):
     template_name = 'create.html'
     model = ToDoModel
     fields = ['title', 'memo', 'priority', 'duedate']
+    success_url = reverse_lazy('list')
+
+class ToDoDelete(DeleteView):
+    template_name = 'delete.html'
+    model = ToDoModel
     success_url = reverse_lazy('list')
